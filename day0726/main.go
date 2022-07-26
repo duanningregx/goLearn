378ff4ad023057c96071aaafddedeeadf32f24f9
duanning <duanning1993@outlook.com>
2022-07-26 17:34:58 +0800

    2022/07/26 切片去重作业

--- day0726/main.go
+++ day0726/main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import "fmt"
 
+func distinct(data []int) []int {
+	var dataMap = make(map[int]int)
+	for _, value := range data {
+		if key, ok := dataMap[value]; ok {
+			dataMap[key]++
+		} else {
+			dataMap[value] = 1
+		}
+	}
+
+	var rel []int
+	for k := range dataMap {
+		rel = append(rel, k)
+	}
+	return rel
+}
+
+func main() {
+	var data = []int{1, 2, 3, 4, 5, 6, 7, 1, 2, 3, 4, 5, 6}
+	rel := distinct(data)
+	fmt.Println(rel)
 }
