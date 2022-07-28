71b95e57af88a779bb5561f32ef1815d6c993896
duanning <duanning1993@outlook.com>
2022-07-28 18:18:29 +0800

    2022/07/28 wait group

--- day0728/main.go
+++ day0728/main.go
@@ -1,9 +1,29 @@
 package main
 
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
 func add(x, y int) int {
 	return x + y
 }
 
-func main() {
+func waitGroup() {
+	wg := sync.WaitGroup{}
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func(x, y int) {
+			defer wg.Done()
+			fmt.Println(x + y)
+		}(i, i+1)
+	}
+	wg.Wait()
+	fmt.Println("exit waitGroup")
+}
 
+func main() {
+	fmt.Println(runtime.NumCPU())
+	waitGroup()
 }
